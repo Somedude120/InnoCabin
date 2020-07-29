3583f792cc7e98e003dbe1763feb168db65be578
DPH <DPH1108@gmail.com>
2020-07-30 01:54:44 +0200

    Fixed the cabinfilereader it can now print out

--- CabinShare/src/App.java
+++ CabinShare/src/App.java
@@ -1,17 +1,52 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Classes.CabinFileReader;
 import Models.CabinFileModel;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Cabin Share!");
-        System.out.println("The calculation will be done from the following folder xxx"); //Remember to add folder for cabintextfiles
-        CabinFileModel cabinObj1 = new CabinFileModel();
-        
+        System.out.println("Please enter a Cabin txt number or write \"all\" for every txt file");
 
-        System.out.println("Name: " + cabinList.get(0).name);
+        Scanner userInput = new Scanner(System.in);
+        int cabinNumber = 0;
+
+        // CabinFileModel cabinObject = new CabinFileModel();
+        ArrayList<CabinFileModel> cabinList = new ArrayList<CabinFileModel>();
+        CabinFileReader cabinFileReader = new CabinFileReader();
+
+        if(userInput.hasNextInt()) {
+            cabinNumber = userInput.nextInt();
+            userInput.close();
+
+            cabinList = cabinFileReader.createCabinObject(cabinList, cabinNumber);
+            //cabinList.add(cabinObject);
+            System.out.println(cabinList.get(0).cabinRentalCost);
+            System.out.println(cabinList.get(0).travelCost);
+
+            for (int i = 0; i < cabinList.size(); i++) {
+                System.out.println(cabinList.get(i).name + " " + cabinList.get(i).distance);
+            }    
+            // System.out.println("Cabin Rental: " + cabinList.get(0).cabinRentalCost);
+            // System.out.println("Travel Cost: " + cabinList.get(0).travelCost);
+            // System.out.println("Name: " + cabinList.get(0).name);
+            // System.out.println("Distance: " + cabinList.get(0).distance);            
+        }
+        // else if(userInput.hasNext("all")) {
+        //     int textNum = 1;
+        //     Files.walk(Paths.get("Cabin/"))
+        //     .filter(Files::isRegularFile)
+        //     .forEach(System.out::println);
+            
+        // }
+        else
+        userInput.close();
+        
+        // System.out.println("Name: " + cabinList.get(0).name);
     }
 }
 
---
+++ CabinShare/src/Classes/CabinFileReader.java
@@ -0,0 +1,52 @@
+package Classes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import Models.CabinFileModel;
+
+/*Structure of file:
+Cabin Rental
+Travel Cost
+
+Name + Distance
+*/
+public class CabinFileReader {
+
+    public CabinFileReader(){
+        
+    }
+
+    public ArrayList<CabinFileModel> createCabinObject(ArrayList<CabinFileModel> cabinList, int textNum) throws FileNotFoundException {
+        
+        //Scan the cabin txt file in
+        if(true){
+            Scanner scanner = new Scanner(new File("src/Cabin/cabin" + textNum + ".txt"));
+            String nameAndDistance;
+
+            while (scanner.hasNextLine()) {
+                if(scanner.hasNextFloat()){
+                    CabinFileModel cabinObj = new CabinFileModel();
+                    cabinObj.cabinRentalCost = Float.parseFloat(scanner.nextLine());
+                    cabinObj.travelCost = Float.parseFloat(scanner.nextLine());
+                    cabinList.add(cabinObj);
+                }
+                else {
+                    CabinFileModel cabinObj = new CabinFileModel();
+                    nameAndDistance = scanner.nextLine();
+                    String[] stringParts = nameAndDistance.split("\\s+");
+                    cabinObj.name = stringParts[0];
+                    cabinObj.distance = stringParts[1];
+                    cabinList.add(cabinObj);
+                }
+            }
+            scanner.close();
+            return cabinList;
+        }
+        else
+        System.out.println(textNum + " is not a number, please enter a valid number");
+        return null;
+    }
+}
