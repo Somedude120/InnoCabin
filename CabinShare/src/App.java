4e6732ad5cce154f9377f879cc7d39b15b066aaf
DPH <DPH1108@gmail.com>
2020-08-01 21:41:05 +0200

    refactored and fixed, constructor injection method

--- CabinShare/src/App.java
+++ CabinShare/src/App.java
@@ -16,22 +16,22 @@ public class App {
         Scanner userInput = new Scanner(System.in);
         int cabinNumber = 0;
 
-        // CabinFileModel cabinObject = new CabinFileModel();
         ArrayList<CabinFileModel> cabinList = new ArrayList<CabinFileModel>();
-        CabinFileReader cabinFileReader = new CabinFileReader();
         CabinFileCalculator cabinFileCalculator = new CabinFileCalculator(cabinList);
-
+        
         if (userInput.hasNextInt()) {
             cabinNumber = userInput.nextInt();
             userInput.close();
-
-            cabinList = cabinFileReader.createCabinObject(cabinList, cabinNumber);
+            
+            CabinFileReader cabinFileReader = new CabinFileReader(cabinList, cabinNumber);
+            cabinList = cabinFileReader.createCabinObject();
+            
             System.out.println("Rental Cost: " + cabinList.get(0).cabinRentalCost);
             System.out.println("Travel Cost: " + cabinList.get(0).travelCost);
             System.out.println("Total Cost: " + cabinFileCalculator.totalCost());
             System.out.println();
 
-            Float travelCostPerPerson = cabinFileCalculator.perPersonCost();
+            cabinFileCalculator.perPersonCost();
             for (int i = 0; i < cabinList.size(); i++) {
                 if(cabinList.get(i).name != null)
                 {
@@ -39,25 +39,9 @@ public class App {
                     + " Travel Cost: " + cabinList.get(i).costPerPerson);
                 }
             }
-
-
-            // System.out.println("Total cost: " + travelCostPerPerson.toString());
-
-            // System.out.println("Cabin Rental: " + cabinList.get(0).cabinRentalCost);
-            // System.out.println("Travel Cost: " + cabinList.get(0).travelCost);
-            // System.out.println("Name: " + cabinList.get(0).name);
-            // System.out.println("Distance: " + cabinList.get(0).distance);
         }
-        // else if(userInput.hasNext("all")) {
-        // int textNum = 1;
-        // Files.walk(Paths.get("Cabin/"))
-        // .filter(Files::isRegularFile)
-        // .forEach(System.out::println);
-
-        // }
         else
+            System.out.println("Not a valid number");
             userInput.close();
-
-        // System.out.println("Name: " + cabinList.get(0).name);
     }
 }
--- CabinShare/src/Classes/CabinFileReader.java
+++ CabinShare/src/Classes/CabinFileReader.java
@@ -15,38 +15,36 @@ Name + Distance
 */
 public class CabinFileReader {
 
-    public CabinFileReader(){
-        
+    CabinFileModel cabinObj;
+    ArrayList<CabinFileModel> cabinList;
+    int textNum;
+
+    public CabinFileReader(ArrayList<CabinFileModel> CabinList, int TextNum) {
+        cabinList = CabinList;
+        textNum = TextNum;
     }
 
-    public ArrayList<CabinFileModel> createCabinObject(ArrayList<CabinFileModel> cabinList, int textNum) throws FileNotFoundException {
-        
-        //Scan the cabin txt file in
-        if(true){
-            Scanner scanner = new Scanner(new File("src/Cabin/cabin" + textNum + ".txt"));
-            String nameAndDistance;
+    public ArrayList<CabinFileModel> createCabinObject() throws FileNotFoundException {
 
-            while (scanner.hasNextLine()) {
-                if(scanner.hasNextFloat()){
-                    CabinFileModel cabinObj = new CabinFileModel();
-                    cabinObj.cabinRentalCost = Float.parseFloat(scanner.nextLine());
-                    cabinObj.travelCost = Float.parseFloat(scanner.nextLine());
-                    cabinList.add(cabinObj);
-                }
-                else {
-                    CabinFileModel cabinObj = new CabinFileModel();
-                    nameAndDistance = scanner.nextLine();
-                    String[] stringParts = nameAndDistance.split("\\s+");
-                    cabinObj.name = stringParts[0];
-                    cabinObj.distance = Float.parseFloat(stringParts[1]);
-                    cabinList.add(cabinObj);
-                }
+        Scanner scanner = new Scanner(new File("src/Cabin/cabin" + textNum + ".txt"));
+        String nameAndDistance;
+
+        while (scanner.hasNextLine()) {
+            if (scanner.hasNextFloat()) {
+                cabinObj = new CabinFileModel();
+                cabinObj.cabinRentalCost = Float.parseFloat(scanner.nextLine());
+                cabinObj.travelCost = Float.parseFloat(scanner.nextLine());
+                cabinList.add(cabinObj);
+            } else {
+                cabinObj = new CabinFileModel();
+                nameAndDistance = scanner.nextLine();
+                String[] stringParts = nameAndDistance.split("\\s+");
+                cabinObj.name = stringParts[0];
+                cabinObj.distance = Float.parseFloat(stringParts[1]);
+                cabinList.add(cabinObj);
             }
-            scanner.close();
-            return cabinList;
         }
-        else
-        System.out.println(textNum + " is not a number, please enter a valid number");
-        return null;
+        scanner.close();
+        return cabinList;
     }
 }
